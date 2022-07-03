4e90123648017b5a22674ea30edca6cdefc123a1
Chirag Mistry <cdmistry97@gmail.com>
2022-07-04 01:24:35 +0530

    Update Dist

--- src/main.tsx
+++ src/main.tsx
@@ -27,7 +27,7 @@ const theme = createTheme({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={'React-Shopping-Cart/dist/'}>
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
